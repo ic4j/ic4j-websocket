cb330233cfc4fcecc1c9dcdef0bfe4ed06cc656a
Roman <roman.dobrik@exilor.com>
2022-12-21 12:44:44 +0100

    Release Candidate 18

--- src/test/java/org/ic4j/management/test/HelloWorldProxy.java
+++ src/test/java/org/ic4j/management/test/HelloWorldProxy.java
@@ -3,14 +3,12 @@ package org.ic4j.management.test;
 import java.util.concurrent.CompletableFuture;
 
 import org.ic4j.agent.annotations.Waiter;
-import org.ic4j.agent.annotations.UPDATE;
 import org.ic4j.agent.annotations.Argument;
 import org.ic4j.candid.annotations.Name;
 import org.ic4j.candid.types.Type;
 
 public interface HelloWorldProxy {
 	
-	@UPDATE
 	@Name("greet")
 	@Waiter(timeout = 30)
 	public CompletableFuture<String> greet(@Argument(Type.TEXT)String name);
